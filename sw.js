2141339f8cf52b631bbe2f5d3bc2aa4693124e35
erayaladag41 <erayaladag@gmail.com>
2026-08-12 09:19:11 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C='poli-isler-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(clients.claim()));
